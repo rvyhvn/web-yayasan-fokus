764274c77cb170ab67f3f494615522136acc27cf
rvyhvn <hanshitrash00@proton.me>
2025-06-28 09:19:21 +0800

    update validator rules

--- app/validators/auth.ts
+++ app/validators/auth.ts
@@ -3,16 +3,25 @@ import vine from '@vinejs/vine'
 export const registerValidator = vine.compile(
   vine.object({
     fullName: vine.string().trim().minLength(8),
-    phone: vine.string().minLength(10).maxLength(13),
-    email: vine.string().email().normalizeEmail({ all_lowercase: true }),
-    password: vine.string().minLength(8).maxLength(32),
+    phone: vine
+      .string()
+      .mobile({ locale: ['id-ID'] })
+      .trim()
+      .minLength(10)
+      .maxLength(13),
+    email: vine
+      .string()
+      .trim()
+      .email()
+      .normalizeEmail({ all_lowercase: true })
+      .unique({ table: 'users', column: 'email' }),
+    password: vine.string().trim().minLength(8).maxLength(32),
   })
 )
 
 export const loginValidator = vine.compile(
   vine.object({
-    email: vine.string().email().normalizeEmail({ all_lowercase: true }),
-    password: vine.string().minLength(8).maxLength(32),
+    email: vine.string().trim().email().normalizeEmail({ all_lowercase: true }),
+    password: vine.string().trim().minLength(8).maxLength(32),
   })
 )
-
